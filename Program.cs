0b1aa7271781ce95291c851d56204de9c48981f0
thayla pereira <thayla.bonfim@etec.sp.gpv.br>
2022-10-31 20:30:05 -0300

    jogo

--- Program.cs
+++ Program.cs
@@ -10,6 +10,23 @@ namespace thayla
     {
         static void Main(string[] args)
         {
+            int a;
+            double b;
+            string nome;
+            string sobrenome;
+            a = 10;
+            b = 11.5;
+            nome = "pato donald";
+
+            Console.WriteLine("digite seu nome:");
+            nome = Console.ReadLine();
+            Console.WriteLine("digite seu sobrenome");
+            sobrenome= Console.ReadLine();
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(nome + " " + sobrenome);
+            Console.ReadKey();
+
         }
     }
 }
